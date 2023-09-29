63716cf73816c96f4fc6a1bf2f4b1c02e2b5f0de
Yildirim Demir <yildirimdemir77@gmail.com>
2023-09-29 15:35:39 +0300

    Update App.jsx

--- x-schools/src/App.jsx
+++ x-schools/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import NavBar from "./ui/NavBar"
+import Footer from "./ui/Footer"
 import About from "./pages/About"
 import Contact from "./pages/Contact"
 import Css from "./pages/CSS"
