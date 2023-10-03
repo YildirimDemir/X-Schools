1abf52dcc0976dff2cace82afea5891181c98d1c
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-10-03 20:00:16 +0300

    sql-csharp-content-add

---
+++ x-schools/src/components/SQL-learn/Sum.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+import sqlSupbase from "../../services/sqlSupabase";
+import SideBar from '../../ui/SideBar';
+import TemplatePage from "../../ui/TemplatePage";
+import CodeSpace from "../../ui/CodeSpace";
+
+export default function Sum() {
+
+    const [SQL, setSQL] = useState([]);
+
+    useEffect(() => {
+        async function getSQL() {
+            const { data, error } = await sqlSupbase
+                .from('sql')
+                .select('*')
+
+            if (error) {
+                console.error(error)
+                throw new Error('SQL contents could not be loaded')
+            }
+
+            setSQL(data);
+        }
+        getSQL();
+    }, []);
+
+    const newSQL = SQL.filter((item) => item.title === 'SQL SUM()');
+
+    return (
+        <div className='page-container'>
+            <div className='page-sidebar'>
+                <SideBar list={SQL} />
+            </div>
+            <div className='page-content'>
+                {newSQL.map((item, index) => (
+                    <div key={index}>
+                        <TemplatePage
+                            title={item.title}
+                            secondTitle="Learn SUM"
+                            about={`${item.about}`}
+                            code={<CodeSpace
+                                title={"SQL Example"}
+                                lang={"text/x-sql"}
+                                readOnly={true}
+                                lineNumbers={true}
+                                enterCode={`${item.code}`} />
+                            } />
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
--- x-schools/src/pages/SQL.jsx
+++ x-schools/src/pages/SQL.jsx
@@ -1,32 +1,28 @@
-import CodeSpace from "../ui/CodeSpace";
-import TemplatePage from "../ui/TemplatePage";
+import { useEffect, useState } from "react";
+import sqlSupbase from "../services/sqlSupabase";
+import SideBar from '../ui/SideBar';
 
-export default function Sql() {
+export default function SQL() {
+
+    const [SQL, setSQL] = useState([]);
+
+    useEffect(() => {
+        async function getSQL() {
+            const { data, error } = await sqlSupbase
+                .from('sql')
+                .select('*')
+
+            if (error) {
+                console.error(error)
+                throw new Error('C# contents could not be loaded')
+            }
+
+            setSQL(data);
+        }
+        getSQL();
+    }, []);
+
     return (
-        <TemplatePage
-            title="SQL Tutorial"
-            secondTitle="Learn SQL"
-            about={`SQL is a standard language for storing, manipulating and retrieving data in databases.
-            Our SQL tutorial will teach you how to use SQL in: MySQL, SQL Server, MS Access, Oracle, Sybase, Informix, Postgres, and other database systems.`}
-            contentColor="darkorange"
-            titleColor="white"
-            secondTitleColor="white"
-            aboutColor="white"
-            code={<CodeSpace
-                title={"SQL Example"}
-                lang={"text/x-sql"}
-                readOnly={true}
-                lineNumbers={true}
-                codeWrapColor="orangered"
-                codeWrapBorderColor="white"
-                titleColor="white"
-                codeAreaColor="white"
-                enterCode={`CREATE TABLE Persons (
-    LastName varchar(255),
-    FirstName varchar(255),
-    Address varchar(255),
-    City varchar(255)
-    PersonID int,
-);`} />} />
+        <SideBar list={SQL} />
     )
 }
