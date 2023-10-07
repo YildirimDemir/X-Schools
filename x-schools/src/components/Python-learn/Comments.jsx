d81608e585f4bc48d1c50c22b5fef268dade90aa
Emircan ÇUBUKCU <emircancbkc@gmail.com>
2023-10-07 14:01:50 +0300

    fix

--- x-schools/src/components/Python-learn/Comments.jsx
+++ x-schools/src/components/Python-learn/Comments.jsx
@@ -1,35 +1,12 @@
-import React, { useEffect, useState } from 'react';
 import SideBar from '../../ui/SideBar';
-import pythonSupabase from '../../services/pythonSupabase';
 import TemplatePage from '../../ui/TemplatePage';
 import CodeSpace from '../../ui/CodeSpace';
 import Loader from '../../ui/Loader';
+import PythonData from '../../Data/PythonData';
 
 export default function Components() {
-    const [python, setPython] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
-        async function getPython() {
-            try {
-                const { data, error } = await pythonSupabase
-                    .from('python')
-                    .select('*');
-
-                if (error) {
-                    console.error(error);
-                    throw new Error('Python contents could not be loaded');
-                }
-
-                setPython(data);
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setIsLoading(false);
-            }
-        }
-        getPython();
-    }, []);
+    const { python, isLoading } = PythonData();
 
     const newPython = python.filter((item) => item.title === 'Python Comments');
 
@@ -43,27 +20,27 @@ export default function Components() {
                     <Loader />
                 ) : (
                     newPython.map((item, index) => (
-                        <TemplatePage
-                        title={item.title}
-                        secondTitle="Learn Python"
-                        about={`${item.about}`}
-                        contentColor="#273469"
-                        titleColor="white"
-                        secondTitleColor="white"
-                        aboutColor="white"
-                        code={
-                        <CodeSpace
-                        title={"Python/JSX Example"}
-                        lang={"jsx"}
-                        readOnly={true}
-                        lineNumbers={true}
-                        codeWrapColor="#30343F"
-                        codeWrapBorderColor="white"
-                        titleColor="white"
-                        codeAreaColor="white"
-                        enterCode={`${item.code}`}
-                        />
-                        }
+                        <TemplatePage key={index}
+                            title={item.title}
+                            secondTitle="Learn Python"
+                            about={`${item.about}`}
+                            contentColor="#273469"
+                            titleColor="white"
+                            secondTitleColor="white"
+                            aboutColor="white"
+                            code={
+                                <CodeSpace
+                                    title={"Python/JSX Example"}
+                                    lang={"jsx"}
+                                    readOnly={true}
+                                    lineNumbers={true}
+                                    codeWrapColor="#30343F"
+                                    codeWrapBorderColor="white"
+                                    titleColor="white"
+                                    codeAreaColor="white"
+                                    enterCode={`${item.code}`}
+                                />
+                            }
                         />
                     ))
                 )}
--- x-schools/src/pages/Python.jsx
+++ x-schools/src/pages/Python.jsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from "react";
-import dataSupabase from "../services/dataSupabase";
 import SideBar from "../ui/SideBar";
+import PythonData from "../Data/PythonData";
 
 export default function Python() {
-  const [python, setPython] = useState([]);
 
-  useEffect(() => {
-    async function getPython() {
-      const { data, error } = await dataSupabase
-        .from('python')
-        .select('*')
-
-      if (error) {
-        console.error(error)
-        throw new Error('Python contents could not be loaded')
-      }
-
-      setPython(data);
-    }
-    getPython();
-  }, [])
+  const { python } = PythonData();
 
   return (
     <div className='page-container'>
